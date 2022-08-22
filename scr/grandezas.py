d211d2d8b2ecab407f6218f86dad3d49ec5c34ea
sheylacantalupo <sheylacantalupo.sc@gmail.com>
2022-08-22 19:29:51 -0300

    testes

---
+++ scr/grandezas.py
@@ -0,0 +1,17 @@
+import pandas
+import json
+
+with open("data.json", encoding='utf-8') as arquivo_json:
+    # objeto python recebendo o carregamento de um arquivo json
+    dados = json.load(arquivo_json)
+
+
+# data = pandas.read_csv("arquivo.csv")
+# grandezas = data["grandeza"].to_list()
+
+
+
+
+
+
+
---
+++ scr/main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter.ttk import *
+from grandezas import dados
+
+#
+# window = Tk()
+# window.title("Conversor de unidades")
+# window.geometry("400x150")
+
+print(dados)
+print(dados[2]['grandeza'])
+
+# # criando combobox
+# combo = Combobox(window)
+# combo['values'] = grandezas
+# combo.place(x=170, y=10)
+#
+#
+# def obter():
+#     resultado = combo.get()
+#     print(resultado)
+#
+#
+# button = Button(text="Calculate", command=obter)
+# button.place(x=170, y=50)
+#
+#
+# window.mainloop()
